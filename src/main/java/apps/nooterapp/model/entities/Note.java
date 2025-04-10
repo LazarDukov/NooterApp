b2648d92baf5ac12f3183b14ddc30585356034d2
LazarDukov <lazarrumenowdukow@gmail.com>
2025-04-11 00:07:33 +0300

    database skeleton created

---
+++ src/main/java/apps/nooterapp/model/entities/Note.java
@@ -0,0 +1,34 @@
+package apps.nooterapp.model.entities;
+
+import apps.nooterapp.model.enums.NoteType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table
+@Entity(name = "notes")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private NoteType type;
+
+    @Column
+    private boolean active;
+
+    @ManyToOne
+    private User user;
+
+
+}
---
+++ src/main/java/apps/nooterapp/model/entities/User.java
@@ -0,0 +1,32 @@
+package apps.nooterapp.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @OneToMany
+    private List<Note> notes;
+
+
+}
