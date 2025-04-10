aa783360721ab589cb8db5c758cdf1d23dd1f61e
LazarDukov <lazarrumenowdukow@gmail.com>
2025-04-10 23:43:16 +0300

    my note project

---
+++ src/main/java/apps/nooterapp/NooterAppApplication.java
@@ -0,0 +1,13 @@
+package apps.nooterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NooterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/apps/nooterapp/NooterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package apps.nooterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NooterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
